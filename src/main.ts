7ed7055bb1da02e808adcf96fbede250e4b59b2f
Voro123 <63288948+Voro123@users.noreply.github.com>
2026-06-23 17:51:56 +0800

    style: load resume enhancement overrides

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { createPinia } from 'pinia'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import './styles/onboarding-dialog.css'
+import './styles/resume-enhancements.css'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 import App from './App.vue'
